4dd430b4cf2a7bcceb4879bff94197160752927e
jason-shen <jason_2000_nz@hotmail.com>
2021-06-04 17:58:04 +0800

    first commit

--- main.go
+++ main.go
@@ -2,20 +2,20 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"github.com/pion/rtcp"
 	"github.com/pion/webrtc/v3"
+	"io"
 	"net/http"
-	"sync"
+	"time"
 )
 
-type pubStream struct {
-	stream *webrtc.TrackLocal
-}
-
-var (
-	trackLocals map[string]*webrtc.TrackLocalStaticRTP
-	listLock    sync.RWMutex
+const (
+	rtcpPLIInterval = time.Second * 3
 )
+
+var localTrackChan = make(chan *webrtc.TrackLocalStaticRTP)
 
 func createSubscribe(w http.ResponseWriter, r *http.Request) {
 	sdp := webrtc.SessionDescription{}
@@ -23,46 +23,7 @@ func createSubscribe(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 
-	// Create new PeerConnection
-	// Everything below is the pion-WebRTC API! Thanks for using it ❤️.
-	// Prepare the configuration
-	//config := webrtc.Configuration{
-	//	ICEServers: []webrtc.ICEServer{
-	//		{
-	//			URLs: []string{"stun:stun.l.google.com:19302"},
-	//		},
-	//	},
-	//}
-	//
-	//// Create a MediaEngine object to configure the supported codec
-	//m := &webrtc.MediaEngine{}
-	//
-	//// Setup the codecs you want to use.
-	//// Only support VP8 and OPUS, this makes our WebM muxer code simpler
-	//if err := m.RegisterCodec(webrtc.RTPCodecParameters{
-	//	RTPCodecCapability: webrtc.RTPCodecCapability{MimeType: "video/VP8", ClockRate: 90000, Channels: 0, SDPFmtpLine: "", RTCPFeedback: nil},
-	//	PayloadType:        96,
-	//}, webrtc.RTPCodecTypeVideo); err != nil {
-	//	panic(err)
-	//}
-	//if err := m.RegisterCodec(webrtc.RTPCodecParameters{
-	//	RTPCodecCapability: webrtc.RTPCodecCapability{MimeType: "audio/opus", ClockRate: 48000, Channels: 0, SDPFmtpLine: "", RTCPFeedback: nil},
-	//	PayloadType:        111,
-	//}, webrtc.RTPCodecTypeAudio); err != nil {
-	//	panic(err)
-	//}
-	//
-	////audioBuilder = samplebuilder.New(10, &codecs.OpusPacket{}, 48000)
-	////videoBuilder = samplebuilder.New(10, &codecs.VP8Packet{}, 90000)
-	//
-	//// Create the API object with the MediaEngine
-	//api := webrtc.NewAPI(webrtc.WithMediaEngine(m))
-	//
-	//// Create a new RTCPeerConnection
-	//peerConnection, err := api.NewPeerConnection(config)
-	//if err != nil {
-	//	panic(err)
-	//}
+	localTrack := <-localTrackChan
 	peerConnection, err := webrtc.NewPeerConnection(webrtc.Configuration{
 		ICEServers: []webrtc.ICEServer{
 			{
@@ -78,9 +39,19 @@ func createSubscribe(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 
-	if _, err = peerConnection.AddTrack(trackLocals["publisher"]); err != nil {
+	rtpSender, err := peerConnection.AddTrack(localTrack)
+	if err != nil {
 		panic(err)
 	}
+
+	go func() {
+		rtcpBuf := make([]byte, 1500)
+		for {
+			if _, _, rtcpErr := rtpSender.Read(rtcpBuf); rtcpErr != nil {
+				return
+			}
+		}
+	}()
 
 	answer, err := peerConnection.CreateAnswer(nil)
 	if err != nil {
@@ -95,7 +66,7 @@ func createSubscribe(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(trackLocals)
+	fmt.Println(localTrackChan)
 	w.Header().Set("Content-Type", "application/json")
 	if _, err := w.Write(output); err != nil {
 		panic(err)
@@ -108,46 +79,6 @@ func createBroadcast(w http.ResponseWriter, r *http.Request) {
 	if err := json.NewDecoder(r.Body).Decode(&sdp); err != nil {
 		panic(err)
 	}
-	// Create new PeerConnection
-	// Everything below is the pion-WebRTC API! Thanks for using it ❤️.
-	// Prepare the configuration
-	//config := webrtc.Configuration{
-	//	ICEServers: []webrtc.ICEServer{
-	//		{
-	//			URLs: []string{"stun:stun.l.google.com:19302"},
-	//		},
-	//	},
-	//}
-	//
-	//// Create a MediaEngine object to configure the supported codec
-	//m := &webrtc.MediaEngine{}
-	//
-	//// Setup the codecs you want to use.
-	//// Only support VP8 and OPUS, this makes our WebM muxer code simpler
-	//if err := m.RegisterCodec(webrtc.RTPCodecParameters{
-	//	RTPCodecCapability: webrtc.RTPCodecCapability{MimeType: "video/VP8", ClockRate: 90000, Channels: 0, SDPFmtpLine: "", RTCPFeedback: nil},
-	//	PayloadType:        96,
-	//}, webrtc.RTPCodecTypeVideo); err != nil {
-	//	panic(err)
-	//}
-	//if err := m.RegisterCodec(webrtc.RTPCodecParameters{
-	//	RTPCodecCapability: webrtc.RTPCodecCapability{MimeType: "audio/opus", ClockRate: 48000, Channels: 0, SDPFmtpLine: "", RTCPFeedback: nil},
-	//	PayloadType:        111,
-	//}, webrtc.RTPCodecTypeAudio); err != nil {
-	//	panic(err)
-	//}
-	//
-	////audioBuilder = samplebuilder.New(10, &codecs.OpusPacket{}, 48000)
-	////videoBuilder = samplebuilder.New(10, &codecs.VP8Packet{}, 90000)
-	//
-	//// Create the API object with the MediaEngine
-	//api := webrtc.NewAPI(webrtc.WithMediaEngine(m))
-	//
-	//// Create a new RTCPeerConnection
-	//peerConnection, err := api.NewPeerConnection(config)
-	//if err != nil {
-	//	panic(err)
-	//}
 
 	peerConnection, err := webrtc.NewPeerConnection(webrtc.Configuration{
 		ICEServers: []webrtc.ICEServer{
@@ -164,20 +95,37 @@ func createBroadcast(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("Connection State has changed %s \n", connectionState.String())
 	})
 
-	peerConnection.OnTrack(func(t *webrtc.TrackRemote, receiver *webrtc.RTPReceiver) {
-		fmt.Println("ontrack")
-		trackLocal := addTrack(t)
-		buf := make([]byte, 1500)
-		for {
-			i, _, err := t.Read(buf)
-			if err != nil {
-				return
+	peerConnection.OnTrack(func(remoteTrack *webrtc.TrackRemote, receiver *webrtc.RTPReceiver) {
+		// Send a PLI on an interval so that the publisher is pushing a keyframe every rtcpPLIInterval
+		// This can be less wasteful by processing incoming RTCP events, then we would emit a NACK/PLI when a viewer requests it
+		go func() {
+			ticker := time.NewTicker(rtcpPLIInterval)
+			for range ticker.C {
+				if rtcpSendErr := peerConnection.WriteRTCP([]rtcp.Packet{&rtcp.PictureLossIndication{MediaSSRC: uint32(remoteTrack.SSRC())}}); rtcpSendErr != nil {
+					fmt.Println(rtcpSendErr)
+				}
 			}
-			if _, err = trackLocal.Write(buf[:i]); err != nil {
-				return
+		}()
+
+		// Create a local track, all our SFU clients will be fed via this track
+		localTrack, newTrackErr := webrtc.NewTrackLocalStaticRTP(remoteTrack.Codec().RTPCodecCapability, "video", "pion")
+		if newTrackErr != nil {
+			panic(newTrackErr)
+		}
+		localTrackChan <- localTrack
+
+		rtpBuf := make([]byte, 1400)
+		for {
+			i, _, readErr := remoteTrack.Read(rtpBuf)
+			if readErr != nil {
+				panic(readErr)
+			}
+
+			// ErrClosedPipe means we don't have any subscribers, this is ok if no peers have connected yet
+			if _, err = localTrack.Write(rtpBuf[:i]); err != nil && !errors.Is(err, io.ErrClosedPipe) {
+				panic(err)
 			}
 		}
-		//fmt.Println("ontrack stream", stream)
 	})
 
 	if err = peerConnection.SetRemoteDescription(sdp); err != nil {
@@ -189,9 +137,17 @@ func createBroadcast(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 
+	// Create channel that is blocked until ICE Gathering is complete
+	gatherComplete := webrtc.GatheringCompletePromise(peerConnection)
+
 	if err = peerConnection.SetLocalDescription(answer); err != nil {
 		panic(err)
 	}
+
+	// Block until ICE Gathering is complete, disabling trickle ICE
+	// we do this because we only can exchange one signaling message
+	// in a production application you should exchange ICE Candidates via OnICECandidate
+	<-gatherComplete
 
 	output, err := json.MarshalIndent(answer, "", "  ")
 	if err != nil {
@@ -203,23 +159,8 @@ func createBroadcast(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func addTrack(t *webrtc.TrackRemote) *webrtc.TrackLocalStaticRTP {
-	listLock.Lock()
-	defer func() {
-		listLock.Unlock()
-	}()
-	fmt.Println("handle E: ", t)
-	trackLocal, err := webrtc.NewTrackLocalStaticRTP(t.Codec().RTPCodecCapability, t.ID(), t.StreamID())
-	if err != nil {
-		panic(err)
-	}
-
-	trackLocals["publisher"] = trackLocal
-	return trackLocal
-}
-
 func main() {
-	trackLocals = map[string]*webrtc.TrackLocalStaticRTP{}
+	// trackLocals = map[string]*webrtc.TrackLocalStaticRTP{}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, r.URL.Path[1:])
 	})
